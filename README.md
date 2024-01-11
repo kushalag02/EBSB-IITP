932395903b9450360930d45db2b2e12854121249
kushal Agarwal <kushalag02@gmail.com>
2024-01-11 22:50:38 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# EBSB-IITP-WEB
+
+Website Development repository for EBSB IIT Patna
+
+## **The website repository for EBSB IIT PATNA**
---
+++ src/index.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  centeredSlides: true,
+  speed: 1500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var typed = new Typed('#typed', {
+  strings: ['EBSB, IIT Patna', 'Ek Bharat Shreshtha Bharat', ' An Indian Government-initiative'],
+  typeSpeed: 50,
+  backSpeed: 60,
+  startDelay: 500,
+  backDelay: 1000,
+  showCursor: false,
+  loop: true,
+});
+
+AOS.init({
+  offset: 200,
+  duration: 900,
+  easing: 'ease',
+  once: true,
+});
+
